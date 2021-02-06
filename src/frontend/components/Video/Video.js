39f2c6c1db488f81d2b24fa222bbace58023ee84
ipsinn <ipsinn@uwaterloo.ca>
2021-02-06 14:57:49 -0500

    Add Youtube video!

--- src/frontend/components/Video/Video.js
+++ src/frontend/components/Video/Video.js
@@ -1,65 +1,61 @@
 import React, { Component } from 'react';
-import VideoPlayer from 'react-video-js-player';
+import YouTube from 'react-youtube';
 
-import './Video.css';
-
-class Video extends Component {
-  player = {};
+class VideoApp extends Component {
+  player = {}
   state = {
-    video: {
-      src: 'http://www.example.com/path/to/video.mp4',
-      poster: 'http://www.example.com/path/to/video_poster.jpg'
-    }
-  };
-
-  onPlayerReady(player) {
-    console.log('Player is ready: ', player);
-    this.player = player;
+      video: {
+          id: "0NDv0Qqq6F0",
+          options: {
+            height: '100%',
+            width: '100%',
+          }
+      }
+  }
+ 
+  onPlayerReady(event){
+    console.log("Player is ready: ", event);
+  }
+ 
+  onVideoPlay(event){
+    console.log("Video played at: ", event);
   }
 
-  onVideoPlay(duration) {
-    console.log('Video played at: ', duration);
+  onVideoPause(event){
+    console.log("Video paused at: ", event);
   }
 
-  onVideoPause(duration) {
-    console.log('Video paused at: ', duration);
+  onVideoPlaybackRateChange(event){
+    console.log("Video playback rate changed: ", event);
   }
 
-  onVideoTimeUpdate(duration) {
-    console.log('Time updated: ', duration);
+  onVideoStateChange(event){
+    console.log("Video state changed: ", event);
   }
 
-  onVideoSeeking(duration) {
-    console.log('Video seeking: ', duration);
+  onVideoEnd(){
+    console.log("Video ended");
   }
-
-  onVideoSeeked(from, to) {
-    console.log(`Video seeked from ${from} to ${to}`);
-  }
-
-  onVideoEnd() {
-    console.log('Video ended');
+  
+  onVideoError(event){
+    console.log("Video error: ", event)
   }
 
   render() {
     return (
-      <div>
-        <VideoPlayer
-          className="Video"
-          controls={true}
-          src={this.state.video.src}
-          poster={this.state.video.poster}
-          onReady={this.onPlayerReady.bind(this)}
-          onPlay={this.onVideoPlay.bind(this)}
-          onPause={this.onVideoPause.bind(this)}
-          onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
-          onSeeking={this.onVideoSeeking.bind(this)}
-          onSeeked={this.onVideoSeeked.bind(this)}
-          onEnd={this.onVideoEnd.bind(this)}
-        />
-      </div>
+      <YouTube
+        videoId={this.state.video.id}
+        className={"Video"}
+        opts={this.state.video.options}
+        onReady={this.onPlayerReady}
+        onPlay={this.onVideoPlay}
+        onPause={this.onVideoPause}
+        onPlaybackRateChange={this.onVideoPlaybackRateChange}
+        onStateChange={this.onVideoStateChange}
+        onEnd={this.onVideoEnd}
+      />
     );
   }
 }
 
-export default Video;
+export default VideoApp;
