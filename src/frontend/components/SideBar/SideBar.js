ffce2b107c8b1eae997d381305fadb420fb64704
ipsinn <ipsinn@uwaterloo.ca>
2021-02-06 20:07:31 -0500

    Add modal for adding new video link

--- src/frontend/components/SideBar/SideBar.js
+++ src/frontend/components/SideBar/SideBar.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
+import ModalAddToPlaylist from '../ModalAddToPlaylist/ModalAddToPlaylist';
 import './SideBar.css';
 
 function SideBar() {
+  const [ isPlaylistModalOpen, setIsPlaylistModalOpen ] = useState(false);
+
   return (
     <div className="SideBar">
       <div className="SideBar-Top">
         <div />
       </div>
       <div className="SideBar-Buttons">
-        <button>Playlist</button>
+        <button className="playlist-button" onClick={() => setIsPlaylistModalOpen(true)}>playlist</button>
+        <button className="exit-button">exit</button>
       </div>
+      <ModalAddToPlaylist
+        isOpen={isPlaylistModalOpen}
+        closeModal={() => setIsPlaylistModalOpen(false)}
+        contentLabel="Add to Playlist"
+      />
     </div>
   );
 }
